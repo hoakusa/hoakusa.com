2e14c3751387e560fef05111cd77aca2ae16817b
hoakusa <hoakusa@gmail.com>
2017-06-12 17:26:28 +0300

    home banner carousel

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -6,24 +6,39 @@ import HomeService from './home.service';
 import AppService from '../../app.service';
 
 class HomeController {
-  constructor(HomeService, AppService) {
+  constructor(HomeService, AppService, $interval) {
     AppService.setTitle('hoakusa |');
     this.pages = HomeService.getPages();
     this.page = this.pages[0];
+
+    this.timeinterval = $interval(this.nextPage.bind(this), 7000);
+    this.$interval = $interval;
   }
 
-  goPrev() {
+  prevPage() {
     let nextId = this.page.id > 0 ? this.page.id - 1 : this.pages.length - 1;
     this.page = this.pages[nextId];
   }
 
-  goNext() {
+  nextPage() {
     let nextId = (this.page.id < this.pages.length - 1) ? this.page.id + 1 : 0;
     this.page = this.pages[nextId];
   }
+
+  goPrev() {
+    this.$interval.cancel(this.timeinterval);
+    this.prevPage();
+    this.timeinterval = this.$interval(this.nextPage.bind(this), 7000);
+  }
+
+  goNext() {
+    this.$interval.cancel(this.timeinterval);
+    this.nextPage();
+    this.timeinterval = this.$interval(this.nextPage.bind(this), 7000);
+  }
 }
 
-HomeController.$inject = ['HomeService', 'AppService'];
+HomeController.$inject = ['HomeService', 'AppService', '$interval'];
 
 export default angular.module('app.home', [uirouter, HomeService, AppService])
   .config(routing)
