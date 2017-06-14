6324c4b3b157a7eb41c4c011e5531e9db839f946
hoakusa <hoakusa@gmail.com>
2017-06-15 01:10:42 +0300

    menu border animation

--- src/app/shared/menu/menu.controller.js
+++ src/app/shared/menu/menu.controller.js
@@ -3,12 +3,16 @@ import MenuService from './menu.service';
 import SocialLinkService from '../social-link.service';
 
 class MenuController {
-  constructor(MenuService, SocialLinkService, $state) {
-    this.$state = $state;
-    this.title = "Alice";
-    this.bio = "UX Designer & Web Developer";
-    this.tabs = MenuService.getMenu();
-    this.links = SocialLinkService.getLinks();
+  constructor(MenuService, SocialLinkService, $state, $timeout) {
+    this.isOpen   = false;
+    this.isClose  = false;
+    this.$state   = $state;
+    this.$timeout = $timeout;
+
+    this.title    = "Alice";
+    this.bio      = "UX Designer & Web Developer";
+    this.tabs     = MenuService.getMenu();
+    this.links    = SocialLinkService.getLinks();
   }
 
   goto(state) {
@@ -17,15 +21,32 @@ class MenuController {
   }
 
   open() {
+    this.isClose = false;
     document.getElementById('menu').style.left = '0';
+    this.isOpen = true;
+    this.$timeout(() => {
+      let arr = document.querySelectorAll('.borderV');
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].style.height = '100%';
+      }
+    }, 1400);
   }
 
   close() {
-    document.getElementById('menu').style.left = '-100%';
+    this.isOpen = false;
+    this.isClose = true;
+
+    this.$timeout(() => {
+      document.getElementById('menu').style.left = '-100%';
+      let arr = document.querySelectorAll('.borderV');
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].style.height = '0';
+      }
+    }, 900);
   }
 }
 
-MenuController.$inject = ['MenuService', 'SocialLinkService', '$state'];
+MenuController.$inject = ['MenuService', 'SocialLinkService', '$state', '$timeout'];
 
 export default angular.module('app.menu', [MenuService, SocialLinkService])
   .controller('MenuController', MenuController)
