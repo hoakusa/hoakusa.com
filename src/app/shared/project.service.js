7f4943587b8740d7e9bb5c0f381633c6d770d61f
hoakusa <hoakusa@gmail.com>
2017-07-05 20:11:40 +0300

    add project to service

--- src/app/shared/project.service.js
+++ src/app/shared/project.service.js
@@ -6,61 +6,129 @@ class ProjectService {
 
     this.projects = [
       {
-        name: 'Project 1',
+        name: 'Plantui App',
         category: 0,
-        tags: ['Interactive', 'Full stack Developer', 'AngularJS', 'React', 'Wordpress CMS'],
+        tags: ['Web', 'Mobile', 'iOS', 'Design', 'Service', 'IoT', 'Interaction', 'UX/UI', 'Photoshop', 'Illustrator'],
         time: {
-          month: 'Feb',
+          month: 'March',
+          year: '2016'
+        },
+        partner: {
+          name: 'Plantui Oy',
+          url: 'http://plantui.com/'
+        },
+        image: '/img/plantui-cover.png',
+        banner_video: '',
+        urls: {
+          behance: 'https://www.behance.net/gallery/54364375/Plantui-App',
+          website: ''
+        },
+        content: {
+          text: 'Apply Internet of Things technique in Plantui Smart device to follow and control plant growth from remote. With Plantui Garden, everyone has fresh vegetable without soil, without take care of all time. Plantui Project is established with Plantui Oy partner and Oulu University of Applied Sciences, Oulu, Finland. The final product includes a website manager and a mobile app service which connect with Plantui device to send plant growth information to user.',
+          images: [
+            '/img/plantui1.png',
+          ]
+        }        
+      },{
+        name: 'Invie House',
+        category: 1,
+        tags: ['Mobile', 'iOS', 'Design', 'Service', 'Branding', 'IoT', 'Interaction', 'UX/UI', 'Photoshop', 'Sketch App'],
+        time: {
+          month: 'December',
+          year: '2016'
+        },
+        partner: {
+          name: 'Invie Group',
+          url: ''
+        },
+        image: '/img/invie-cover.png',
+        banner_video: '',
+        urls: {
+          behance: 'https://www.behance.net/gallery/54423973/Invie-App',
+          website: ''
+        },
+        content: {
+          text: 'Start with saving energy ideal in Avanto kickstarter program, Inive House bring a smart solution for future house to follow and control electric furniture via Invie application. Apply Internet of Things technique in Invie Plug, we bring a wireless house control system for everyone. With Invie House, people are free to control electric device from remote, also observe and manage electric consumption usage. With Invie App, everything is under your control.',
+          images: [
+            '/img/invie1.png',
+          ]
+        }        
+      },{
+        name: 'Alphabet Creative Studio',
+        category: 0,
+        tags: ['Web', 'Design', 'Wordpress', 'AngularJS', 'Javascript', 'Animation', 'UX/UI', 'Responsive', 'Photoshop', 'Illustrator'],
+        time: {
+          month: 'June',
+          year: '2017'
+        },
+        partner: {
+          name: '',
+          url: ''
+        },
+        image: '/img/alphabet-cover.png',
+        banner_video: '',
+        urls: {
+          behance: 'https://www.behance.net/gallery/54391931/Alphabet-Studio',
+          website: 'https://codepen.io/hoakusa/full/ZKRzZX/'
+        },
+        content: {
+          text: 'Alphabet Creative Studio provides website template at the intersection of technology, design, culture, and brand stories. The website exhibits design products made by Alphabet Studio. With bright color set, we believe this website is appropriate landing page for various companes and indie studios. After finish design phase with Photoshop and Illustrator, a demo website was made by AngularJS and available to visit around. Hope people like it.',
+          images: [
+            '/img/alphabet1.png',
+          ]
+        }        
+      },{
+        name: 'Kiem.fi 2017',
+        category: 0,
+        tags: ['Web', 'Design', 'Wordpress', 'Wordpress API', 'React', 'Animation', 'UX/UI', 'Responsive', 'Kiem Oy', 'Photoshop', 'Illustrator'],
+        time: {
+          month: 'May',
           year: '2017'
         },
         partner: {
           name: 'Kiem Oy',
           url: 'http://kiem.fi'
         },
-        image: 'https://image.ibb.co/cdT2tF/G743bff.jpg',
+        image: '/img/kiem2017-cover.png',
         banner_video: '',
         urls: {
-          bahance: '/',
-          website: '/'
+          behance: 'https://www.behance.net/gallery/54369059/Kiemfi-2017',
+          website: 'http://kiem.fi'
         },
         content: {
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin purus sapien, sit amet viverra urna laoreet in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum porttitor urna, ac sollicitudin diam fringilla in. Cras at sem in ex interdum pharetra. Proin viverra congue justo, sit amet tincidunt quam dapibus ac.',
+          text: 'Kiem Ravintola is a Chinese Restaurant in Oulu areas, Finland. In remake version, new website interface focuses on highlight Menu list and buffet lounas service. About backend, we continue using Wordpress as manager tool for customize style purpose and update posts. In front-end, because Wordpress has opened REST API from version 4.7 new client can connect with database through request via React-axios, hence, exchange data will pass through request then handle with React components.',
           images: [
-            'https://image.ibb.co/cdT2tF/G743bff.jpg',
-            'https://image.ibb.co/gQ21na/ARiOahH.jpg',
-            'https://image.ibb.co/dHOkDF/o3hIuqr.jpg'
+            '/img/kiem20171.png',
           ]
         }        
       },{
-        name: 'Web Service',
-        category: 1,
-        tags: ['Interactive', 'Full stack Developer', 'AngularJS', 'React', 'Wordpress CMS'],
+        name: 'Minhshop E-commercial',
+        category: 0,
+        tags: ['Web', 'Design', 'E-commercial', 'Interaction', 'UX/UI', 'Responsive', 'Photoshop', 'Illustrator'],
         time: {
-          month: 'Feb',
-          year: '2016'
+          month: 'March',
+          year: '2017'
         },
         partner: {
-          name: 'Kiem Oy',
-          url: 'http://kiem.fi'
+          name: 'Minhshop',
+          url: ''
         },
-        image: 'https://image.ibb.co/gQ21na/ARiOahH.jpg',
+        image: '/img/minhshop-cover.png',
         banner_video: '',
         urls: {
-          bahance: '/',
-          website: '/'
+          behance: 'https://www.behance.net/gallery/54370019/Minh-shop',
+          website: ''
         },
         content: {
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin purus sapien, sit amet viverra urna laoreet in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum porttitor urna, ac sollicitudin diam fringilla in. Cras at sem in ex interdum pharetra. Proin viverra congue justo, sit amet tincidunt quam dapibus ac.',
+          text: 'The purpose of the project is to design an e-commercial comestic website for Minhshop in Hanoi. Their online shopping activity could increase their profit and attract more attention via social media network. The shop website transmits necessary information about cosmetic products in different categories. Grouping products provides a powerful solution for quickly view and selection that help administrator could effortlessly update product details. More than promoting local store activities, website product can preserve a successful long term relationship between shop and customer.',
           images: [
-            'https://image.ibb.co/cdT2tF/G743bff.jpg',
-            'https://image.ibb.co/gQ21na/ARiOahH.jpg',
-            'https://image.ibb.co/dHOkDF/o3hIuqr.jpg'
+            '/img/minhshop1.png',
           ]
-        }
+        }        
       },{
-        name: 'iOS/Android Application',
-        category: 2,
-        tags: ['Mobile Interaction', 'Prototype', 'Invision', 'Frame.js', 'IoT app'],
+        name: 'Kiem.fi 2015',
+        category: 0,
+        tags: ['Web', 'Wordpress', 'AngularJS', 'Animation', 'UX/UI', 'Kiem Oy'],
         time: {
           month: 'Feb',
           year: '2015'
@@ -69,24 +137,81 @@ class ProjectService {
           name: 'Kiem Oy',
           url: 'http://kiem.fi'
         },
-        image: 'https://image.ibb.co/dHOkDF/o3hIuqr.jpg',
-        banner_video: '',
+        image: '/img/kiem2015-cover.png',
+        banner_video: '/img/kiem2015.mp4',
         urls: {
-          bahance: '/',
-          website: '/'
+          behance: 'https://www.behance.net/gallery/54369059/Kiemfi-2015',
+          website: 'http://kiem.fi'
         },
         content: {
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin purus sapien, sit amet viverra urna laoreet in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum porttitor urna, ac sollicitudin diam fringilla in. Cras at sem in ex interdum pharetra. Proin viverra congue justo, sit amet tincidunt quam dapibus ac.',
+          text: 'Kiem Ravintola is a Chinese Restaurant in Oulu areas, Finland. In Kiem site, customers feel relax to search information details, list of foods and music event in Kiem Pub every weekend. Build by Wordpress CMS, the admin page provides a smooth manager tool for modifying and update posts.',
           images: [
-            'https://image.ibb.co/cdT2tF/G743bff.jpg',
-            'https://image.ibb.co/gQ21na/ARiOahH.jpg',
-            'https://image.ibb.co/dHOkDF/o3hIuqr.jpg'
+            '/img/kiem2015-cover.png',
+            '/img/kiem20152.png',
+          ]
+        }        
+      },{
+        name: 'OutdoorAR ver2.0',
+        category: 0,
+        tags: ['Mobile', 'Design', 'Animation', 'Interaction', 'UX/UI', 'Service Design', 'Suprview Oy', 'Augmented Reality', 'Photoshop', 'Illustrator'],
+        time: {
+          month: 'Aug',
+          year: '2015'
+        },
+        partner: {
+          name: 'Suprview Oy',
+          url: 'http://suprview.fi'
+        },
+        image: '/img/indoor-cover.png',
+        banner_video: '/img/indoor-cover.mp4',
+        urls: {
+          behance: 'https://www.behance.net/gallery/54341707/OutdoorAR-ver20',
+          website: 'https://play.google.com/store/apps/details?id=com.suprview.prefabshouses&hl=en'
+        },
+        content: {
+          text: 'OUTDOOR AR offers you a gorgeous way to view your house in its glory. In the second version, OUTDOOR AR provides an intuitive technique to discovery both outside and inside your house. With walk through view, customers feel relax to customize home style by own ideal.',
+          images: [
+            '/img/indoor1.png',
+            '/img/indoor2.gif',
+            '/img/indoor-cover.png',
+            '/img/indoor4.gif',
+            '/img/indoor5.png'
+          ]
+        }
+      },{
+        name: 'OutdoorAR',
+        category: 0,
+        tags: ['Mobile', 'Design', 'Animation', 'Interaction', 'UX/UI', 'Service Design', 'Suprview Oy', 'Augmented Reality', 'Photoshop', 'Illustrator'],
+        time: {
+          month: 'May',
+          year: '2015'
+        },
+        partner: {
+          name: 'Suprview Oy',
+          url: 'http://suprview.fi'
+        },
+        image: '/img/outdoorAR-cover.png',
+        banner_video: '/img/outdoorAR.mp4',
+        urls: {
+          behance: 'https://www.behance.net/gallery/54213967/OutdoorAR',
+          website: 'https://play.google.com/store/apps/details?id=com.suprview.prefabshouses&hl=en'
+        },
+        content: {
+          text: 'OUTDOOR AR offers you a gorgeous way to view your house in its glory. Under the effect of our amazing software with the use of cutting-edge Augmented Reality technology, 3D model of building will be smoothly visualized in the real environment. There are some unique features such as changing the size or even colors of the building. Our product is known as AR catalog that helps prefabricated house manufacturers to get notification from potential buyers anywhere in the world without a sales person initial contacts. #outdoorAR		#mobileservice		#ARapp	#interactivedesign',
+          images: [
+            '/img/outdoorAR-cover.png',
+            '/img/outdoorAR2.png',
+            '/img/outdoorAR3.png',
+            '/img/outdoorAR4.gif',
+            '/img/outdoorAR5.png',
+            '/img/outdoorAR9.gif',
+            '/img/outdoorAR10.png',
           ]
         }
       },{
         name: 'Instagroom',
         category: 0,
-        tags: ['Mobile app', 'Javascript', 'Service', 'Ionic', 'AngularJS'],
+        tags: ['Mobile', 'Javascript', 'Service', 'Ionic', 'AngularJS'],
         time: {
           month: 'Oct',
           year: '2015'
@@ -98,7 +223,7 @@ class ProjectService {
         image: '/img/instagroom-cover.png',
         banner_video: '/img/instagroom.mp4',
         urls: {
-          bahance: '',
+          behance: '',
           website: 'https://github.com/hoakusa/Instagram_client'
         },
         content: {
@@ -122,7 +247,7 @@ class ProjectService {
         image: '/img/alex-cover.png',
         banner_video: '/img/alex-cover.mp4',
         urls: {
-          bahance: 'https://www.behance.net/gallery/54387205/Alex-in-Wonderland',
+          behance: 'https://www.behance.net/gallery/54387205/Alex-in-Wonderland',
           website: 'http://globalgamejam.org/2015/games/alex-wonderland'
         },
         content: {
@@ -132,9 +257,63 @@ class ProjectService {
           ]
         }
       },{
+        name: 'Nuoriso Liider: Young Thinkers & Doers',
+        category: 2,
+        tags: ['Marketing plan', 'Demola Oulu', 'Web'],
+        time: {
+          month: 'April',
+          year: '2015'
+        },
+        partner: {
+          name: 'Nouseva Rannikkoseutu ry',
+          url: 'https://www.nousevarannikkoseutu.fi/fi/etusivu'
+        },
+        image: '/img/nuoriso-cover.jpg',
+        banner_video: '',
+        urls: {
+          behance: '',
+          website: 'https://www.nousevarannikkoseutu.fi/fi/etusivu'
+        },
+        content: {
+          text: 'The purpose of the project was to build an innovation platform for the YouthLeader program which is aimed at young people aged 13-20 living in one of the 7 municipalities of Hailuoto, Liminka, Tyrnävä, Lumijoki, Siikajoki, Raahe and Pyhäjoki. The program provides funding up to 500 euros to small groups (minimum 3 people) of young people to support their ideas for action and change in their home region. The YouthLeader program is organised by Nouseva Rannikkoseutu (Norsu) ry, and at present, the application process for funding is carried out through the Norsu website www.nousevarannikkoseutu.fi.',
+          images: [
+            '/img/nuoriso-cover.jpg',
+            '/img/nuoriso1.jpg',
+            '/img/nuoriso2.jpg',
+            '/img/nuoriso3.jpg',
+          ]
+        }
+      },{
+        name: 'Quote Machine',
+        category: 0,
+        tags: ['Web', 'Free Code Camp', 'Javascript', 'Web Animation', 'Social Embeded'],
+        time: {
+          month: 'June',
+          year: '2017'
+        },
+        partner: {
+          name: '',
+          url: ''
+        },
+        image: '/img/quote-cover.png',
+        banner_video: '',
+        urls: {
+          behance: '',
+          website: 'https://codepen.io/hoakusa/full/GEKzKN/'
+        },
+        content: {
+          text: 'With Quote Machine, people will get random quotation by genius in the world. In my application, precious quotation resources came from popular book authors. In additional, visitors are welcome to tweet out and sharing favourite quote through twitter network.',
+          images: [
+            '/img/quote1.png',
+            '/img/quote2.png',
+            '/img/quote3.png',
+            '/img/quote4.png'
+          ]
+        }
+      },{
         name: 'Simon Game',
         category: 0,
-        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web app'],
+        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web'],
         time: {
           month: 'June',
           year: '2017'
@@ -146,7 +325,7 @@ class ProjectService {
         image: '/img/simon-cover.png',
         banner_video: '/img/simon.mp4',
         urls: {
-          bahance: '',
+          behance: '',
           website: 'https://codepen.io/hoakusa/full/wevVKw/'
         },
         content: {
@@ -160,7 +339,7 @@ class ProjectService {
       },{
         name: 'Tic tac toe',
         category: 0,
-        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web app'],
+        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web'],
         time: {
           month: 'June',
           year: '2017'
@@ -172,7 +351,7 @@ class ProjectService {
         image: '/img/tic-cover.png',
         banner_video: '/img/tic.mp4',
         urls: {
-          bahance: '',
+          behance: '',
           website: 'https://codepen.io/hoakusa/full/KqKRzg/'
         },
         content: {
@@ -187,7 +366,7 @@ class ProjectService {
       },{
         name: 'Calculator',
         category: 0,
-        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web app'],
+        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web'],
         time: {
           month: 'June',
           year: '2017'
@@ -199,7 +378,7 @@ class ProjectService {
         image: '/img/calculator.png',
         banner_video: '',
         urls: {
-          bahance: '',
+          behance: '',
           website: 'https://codepen.io/hoakusa/full/yXLPby/'
         },
         content: {
@@ -211,7 +390,7 @@ class ProjectService {
       },{
         name: 'Pomodoro Clock',
         category: 0,
-        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web app'],
+        tags: ['Game', 'Javascript', 'Free Code Camp', 'Web'],
         time: {
           month: 'June',
           year: '2017'
@@ -223,7 +402,7 @@ class ProjectService {
         image: '/img/pomodoro.png',
         banner_video: '',
         urls: {
-          bahance: '',
+          behance: '',
           website: 'https://codepen.io/hoakusa/full/VWwyGo/'
         },
         content: {
@@ -232,7 +411,7 @@ class ProjectService {
             '/img/pomodoro.png'
           ]
         }
-      }   
+      }  
     ];
   }
 
